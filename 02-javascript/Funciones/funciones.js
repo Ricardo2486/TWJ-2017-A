eeb119d1e5b121e890c0ca4c934089bde9b5c2d8
Ricardo2486 <cristian.r.c@hotmail.es>
2017-05-03 14:59:50 -0500

    met

---
+++ 02-javascript/Funciones/funciones.js
@@ -0,0 +1,47 @@
+
+//void
+
+function holaMundo(){
+    console.log('Hola Mundo');
+}
+holaMundo();
+
+//El doble de un entero
+function doble(numero){
+    return numero*2;
+}
+console.log(doble(5));
+
+//Devuelva mult 2 num
+function multiplicar2 (a,b){
+    return a*b;
+}
+console.log(multiplicar2(4,3));
+//se guarda el valor de la variable
+var dos = multiplicar2(1,2);
+//si no ponemos sin parentesis se guarda la funcion
+var multiplicacion = multiplicar2;
+console.log(multiplicacion (6,4));
+
+//cuando no se pone ningun nombre a la funcion se llaman funciones anonimas
+var hola = function (){
+    console.log('hola');
+}
+hola(); //hola
+console.log(hola()); //undefined
+
+var resultado = hola();
+console.log(resultado);
+
+function hazAlgo (algo){
+    algo();
+}
+hazAlgo(function () { console.log('Haz Algo')});
+
+function transformarNumero(numero, transformacion) {
+    transformacion(numero);
+}
+transformarNumero(5,function (numero) {
+    return numero*2;
+
+})
