228af93418ffa0334324230e60ac3ddf1a0d5ca6
Ricardo2486 <cristian.r.c@hotmail.es>
2017-05-09 16:31:10 -0500

    TempladeStrings

---
+++ 02-javascript/TempladeStrings/templadeStrings.js
@@ -0,0 +1,16 @@
+/**
+ * Created by User on 09/05/2017.
+ */
+//Templade Strings
+
+
+//
+var htmlES6 = `<h1>Hola</h1>
+<p>Hola amigos como esttan </p>`
+
+htmlES6
+
+var nombreUsuario = "Cristian Cabascango"
+
+console.log (`<h1>Hola ${nombreUsuario} </h1>
+<p>Hola amigos como estan </p>`)
--- 02-javascript/app.js
+++ 02-javascript/app.js
@@ -3,3 +3,33 @@
  */
 
 console.log('Hola Mundo'); //.log es una funcion que admite strings
+
+//acceder a los variables dentro del objeto el nombre del objeto con .
+var cristian ={
+    nombre:"Cristian",
+    apellido: "Cabascango",
+    imprimir: function (){
+        console.log(cristian.nombre,cristian.apellido);
+}
+}
+
+cristian.imprimir();
+//para acceder a las variables dentro del objeto con this
+var ricardo ={
+    nombre:"Ricardo",
+    apellido: "Cabascango",
+    fechaNacimiento: new Date(),
+    imprimir: function (){
+        console.log(this.nombre,this.apellido);
+    },
+    año: function (){
+        return this.fechaNacimiento.getFullYear();//Obtener el año actual del sistema
+    },
+    diferenciaEdad:function (edadAComparar){
+        return Math.abs(this.fechaNacimiento.getFullYear()-edadAComparar);
+    }
+}
+ricardo.imprimir();
+console.log(ricardo.año());
+console.log(ricardo.diferenciaEdad(2030));
+
