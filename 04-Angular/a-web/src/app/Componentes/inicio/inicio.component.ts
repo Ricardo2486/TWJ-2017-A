f054626b096c2d40039cf8a3d900fb5dca4c71a1
Ricardo2486 <cristian.r.c@hotmail.es>
2017-05-30 16:58:58 -0500

    ngFor

--- 04-Angular/a-web/src/app/Componentes/inicio/inicio.component.ts
+++ 04-Angular/a-web/src/app/Componentes/inicio/inicio.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  nombre:string = "Cristian";
+
+  arregloUsuarios =[{
+    nombre: "Rafael",
+    apellido: "Eguez"
+  },
+    {
+      nombre: "Adrian",
+      apellido: "Eguez"
+    },
+    {
+      nombre: "Juan",
+      apellido: "Eguez"
+    },]
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+
+
+}
+function cambiarNombre ():void{
+
+  this.nombre = "Rafico a Lenin";
+}
+function cambiarOtroNombre()
+{
+  this.nombre = "Lenin A Rafico";
+}
+
+function cambiarNombreInput(nombreEtiqueta){
+  console.log(nombreEtiqueta.value);
+  console.log(nombreEtiqueta.type);
+  console.log(nombreEtiqueta.placeholder);
+  this.nombre=nombreEtiqueta.value;
 }
